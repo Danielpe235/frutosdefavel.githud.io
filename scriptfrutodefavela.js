806fd63cbfdf98a12763cefa0065387996abe651
Danielpe235 <dandannielpraime@gmail.com>
2019-09-14 23:29:24 -0300

    alteracoes 1

---
+++ scriptfrutodefavela.js
@@ -0,0 +1,7 @@
+var paragrafo_sn = document.querySelector("p#p1");
+var paragrafo_ps = document.querySelector("p#p2");
+var paragrafo_rd = document.querySelector("p#p3");
+
+paragrafo_sn.innerHTML="Finanças é responsa! Levamos essa parada muito a sério e fazemos questão de mostrar como a Gerando Falcões utiliza os recursos, mede o impacto e fala sobre as nossas metas para transformar as favelas brasileiras."
+paragrafo_ps.innerHTML="Finanças é responsa! Levamos essa parada muito a sério e fazemos questão de mostrar como a Gerando Falcões utiliza os recursos, mede o impacto e fala sobre as nossas metas para transformar as favelas brasileiras."
+paragrafo_rd.innerHTML="Finanças é responsa! Levamos essa parada muito a sério e fazemos questão de mostrar como a Gerando Falcões utiliza os recursos, mede o impacto e fala sobre as nossas metas para transformar as favelas brasileiras."
