844df1391aaa3bf221a6f58067f3399b27b47575
Danielpe235 <dandannielpraime@gmail.com>
2019-09-16 22:53:52 -0300

    alteracoes

--- scriptfrutodefavela.js
+++ scriptfrutodefavela.js
@@ -1,7 +1,12 @@
 var paragrafo_sn = document.querySelector("p#p1");
 var paragrafo_ps = document.querySelector("p#p2");
 var paragrafo_rd = document.querySelector("p#p3");
+var txt_ft1 = document.querySelector("p#ft1");
+var txt_ft2 = document.querySelector("p#ft2");
+var txt_ft3 = document.querySelector("p#ft3");
 
-paragrafo_sn.innerHTML="Finanças é responsa! Levamos essa parada muito a sério e fazemos questão de mostrar como a Gerando Falcões utiliza os recursos, mede o impacto e fala sobre as nossas metas para transformar as favelas brasileiras."
-paragrafo_ps.innerHTML="Finanças é responsa! Levamos essa parada muito a sério e fazemos questão de mostrar como a Gerando Falcões utiliza os recursos, mede o impacto e fala sobre as nossas metas para transformar as favelas brasileiras."
-paragrafo_rd.innerHTML="Finanças é responsa! Levamos essa parada muito a sério e fazemos questão de mostrar como a Gerando Falcões utiliza os recursos, mede o impacto e fala sobre as nossas metas para transformar as favelas brasileiras."
+paragrafo_sn.innerHTML="Finanças é responsa! Levamos essa parada muito a sério e fazemos questão de mostrar como a Gerando Falcões utiliza os recursos, mede o impacto e fala sobre as nossas metas para transformar as favelas brasileiras.";
+paragrafo_ps.innerHTML="Finanças é responsa! Levamos essa parada muito a sério e fazemos questão de mostrar como a Gerando Falcões utiliza os recursos, mede o impacto e fala sobre as nossas metas para transformar as favelas brasileiras.";
+paragrafo_rd.innerHTML="Finanças é responsa! Levamos essa parada muito a sério e fazemos questão de mostrar como a Gerando Falcões utiliza os recursos, mede o impacto e fala sobre as nossas metas para transformar as favelas brasileiras.";
+txt_ft1.innerHTML="Finanças é responsa! Levamos essa parada muito a sério e fazemos questão de mostrar como a Gerando Falcões utiliza os recursos, mede o impacto e fala sobre as nossas metas para transformar as favelas brasileiras.";
+txt_ft2.innerHTML="Finanças é responsa! Levamos essa parada muito a sério e fazemos questão de mostrar como a Gerando Falcões utiliza os recursos, mede o impacto e fala sobre as nossas metas para transformar as favelas brasileiras.";
